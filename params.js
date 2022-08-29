2cf8c8af0f108d68d6932bf38a165f62bf5b7d3d
William TA <willt29000@gmail.com>
2022-08-29 21:36:48 +0200

    chore: fix eslint warnings

--- params.js
+++ params.js
@@ -1,14 +1,12 @@
-const HOST = '0.0.0.0'
-const PORT = 3004
-
+const HOST = '0.0.0.0';
+const PORT = 3004;
 
 const params = {
   server: {
     host: HOST,
     port: PORT,
-    url: `http://${HOST}:${PORT}` 
+    url: `http://${HOST}:${PORT}`,
   },
-}
+};
 
-module.exports = params
-
+module.exports = params;
