0d605505e0ab252b4cd8d09697adf0f1b8e0c874
William TA <wta@student.42.fr>
2022-08-31 12:11:42 +0200

    chore: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const path = require('path');
 module.exports = {
   entry: './src/client/index.jsx',
 
+  devtool: 'inline-source-map',
+
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    hot: true,
+  },
+
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
