08e28a780dbb5bd022f7bc360faec279273903b6
William TA <wta@student.42.fr>
2022-09-01 17:41:30 +0200

    feat: implement Player base class

---
+++ src/server/entities/Player.js
@@ -0,0 +1,31 @@
+class Player {
+  constructor(name) {
+    this._name = name;
+    this._alive = true;
+    this._current_sequence = 0;
+  }
+
+  nextSequence() {
+    this._current_sequence += 1;
+    return this._current_sequence;
+  }
+
+  reset() {
+    this._alive = true;
+    this._current_sequence = 0;
+  }
+
+  kill() {
+    this._alive = false;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get alive() {
+    return this._alive;
+  }
+}
+
+export default Player;
