f4d2ed5e85375b3de35c89d53b0dd528c57a2c23
William TA <wta@student.42.fr>
2022-09-02 16:39:54 +0200

    chore(test): add multiple socket clients game start test

--- test/server/socket-io/game/start.test.js
+++ test/server/socket-io/game/start.test.js
@@ -28,15 +28,58 @@ describe('Game starting', () => {
 
   after(() => testServer.stop());
 
-  it('should start a game', (done) => {
+  it('should start a game with one player', (done) => {
     clientSocket.on('game:start', ({ type, gameId }) => {
       assert.equal(type, 'game/start');
       expect(gameId.startsWith('1234')).to.be.true;
       done();
     });
 
-    clientSocket.emit('red-tetris:register', { name: 'Bruce Wayne ' });
+    clientSocket.emit('red-tetris:register', { name: 'Bruce Wayne' });
     clientSocket.emit('room:create', { roomId: '1234' });
     clientSocket.emit('game:start', { roomId: '1234' });
+  });
+
+  it('should start a game with multiple players', (done) => {
+    const clarkSocket = new Client(`http://${testServer.host}:${testServer.port}`);
+    const spiderManSocket = new Client(`http://${testServer.host}:${testServer.port}`);
+    const ironManSocket = new Client(`http://${testServer.host}:${testServer.port}`);
+
+    clientSocket.emit('red-tetris:register', { name: 'Bruce Wayne' });
+    clarkSocket.emit('red-tetris:register', { name: 'Clark Kent' });
+    spiderManSocket.emit('red-tetris:register', { name: 'Spider Man' });
+    ironManSocket.emit('red-tetris:register', { name: 'Iron Man' });
+
+    clientSocket.emit('room:create', { roomId: '1234', maxPlayers: 4 });
+
+    setTimeout(() => {
+      clarkSocket.emit('room:join', { roomId: '1234' });
+      spiderManSocket.emit('room:join', { roomId: '1234' });
+      ironManSocket.emit('room:join', { roomId: '1234' });
+
+      clientSocket.emit('game:start', { roomId: '1234' });
+
+      const gameStartResolver = (socket) => new Promise((resolve) => {
+        socket.on('game:start', (arg) => {
+          assert.equal(arg.type, 'game/start');
+          expect(arg.gameId.startsWith('1234#')).to.be.true;
+          resolve(arg.gameId);
+        });
+      });
+
+      const promises = [
+        clientSocket,
+        clarkSocket,
+        spiderManSocket,
+        ironManSocket,
+      ].map(gameStartResolver);
+
+      Promise.all(promises)
+        .then((gameIds) => {
+          // assert gameIds uniqueness
+          assert.equal(new Set(gameIds).size, promises.length);
+          done();
+        });
+    }, 250);
   });
 });
