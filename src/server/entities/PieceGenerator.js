88b2b8857f15a6e1af97cc8996710297e96a8622
William TA <wta@student.42.fr>
2022-09-02 12:29:45 +0200

    feat: implement PieceGenerator class

---
+++ src/server/entities/PieceGenerator.js
@@ -0,0 +1,50 @@
+export const SEQUENCE_SIZE = 7;
+export const FIRST_SEQUENCE = 'IJLT';
+export const DEFAULT_SEQUENCE = 'IJLOSTZ';
+
+export const shuffle = (array) => {
+  const result = [...array];
+
+  for (let i = 0; i < result.length; i += 1) {
+    const j = parseInt((Math.random() * 100) % (i + 1), 10);
+    const tmp = result[i];
+    result[i] = result[j];
+    result[j] = tmp;
+  }
+  return result;
+};
+
+const generateSequence = (isFirstSequence = false) => {
+  const sequence = [];
+  if (isFirstSequence) {
+    sequence.push(shuffle(FIRST_SEQUENCE).pop());
+  }
+
+  const shuffledPieces = shuffle(DEFAULT_SEQUENCE);
+  while (sequence.length < SEQUENCE_SIZE) {
+    const piece = shuffledPieces.pop();
+    if (piece !== sequence[0]) {
+      sequence.push(piece);
+    }
+  }
+  return sequence;
+};
+
+class PieceGenerator {
+  constructor() {
+    this._sequences = [];
+  }
+
+  reset() {
+    this._sequences = [];
+  }
+
+  deal(sequenceIndex) {
+    while (this._sequences.length <= sequenceIndex) {
+      this._sequences.push(generateSequence(sequenceIndex === 0));
+    }
+    return this._sequences[sequenceIndex];
+  }
+}
+
+export default PieceGenerator;
---
+++ test/server/entities/piece-generator.test.js
@@ -0,0 +1,47 @@
+import { expect } from 'chai';
+import {
+  describe,
+  it,
+} from 'mocha';
+
+import PieceGenerator, { DEFAULT_SEQUENCE, FIRST_SEQUENCE, SEQUENCE_SIZE } from '../../../src/server/entities/PieceGenerator';
+
+describe('Piece generator', () => {
+  it('should deal one sequence', (done) => {
+    const generator = new PieceGenerator();
+
+    const sequence = generator.deal(0);
+
+    expect(sequence.length).to.equal(7);
+    done();
+  });
+
+  it('should only give IJLT', (done) => {
+    const expectedPieces = new Set(FIRST_SEQUENCE.split(''));
+    const generator = new PieceGenerator();
+
+    for (let i = 0; i < 1000; i += 1) {
+      const sequence = generator.deal(0);
+      generator.reset(0);
+      expect(expectedPieces.has(sequence[0])).to.be.true;
+    }
+    done();
+  });
+
+  it('should draw every pieces', (done) => {
+    const expectedPieces = new Set(DEFAULT_SEQUENCE.split(''));
+    const generator = new PieceGenerator();
+
+    const sequence = generator.deal(0);
+
+    expect(expectedPieces.size).to.equal(SEQUENCE_SIZE);
+
+    sequence.forEach((piece) => {
+      expect(expectedPieces.has(piece)).to.be.true;
+      expectedPieces.delete(piece);
+    });
+
+    expect(expectedPieces.size).to.equal(0);
+    done();
+  });
+});
