5e42afd85ec94e31ea6e2499bd5818749d00511c
William TA <wta@student.42.fr>
2022-09-06 11:20:38 +0200

    chore(webpack): create separate prod/dev config

---
+++ webpack.prod.js
@@ -0,0 +1,11 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin(),
+  ],
+});
