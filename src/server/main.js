af17a04ea912ebfad6c3aa886622de842f1501d2
William TA <wta@student.42.fr>
2022-09-08 14:00:56 +0200

    refactor: remove useless console.log

--- src/server/main.js
+++ src/server/main.js
@@ -1,4 +1,4 @@
 import params from '../../params';
 import createServer from './index';
 
-createServer(params.server).then(() => console.log('not yet ready to play tetris with U ...'));
+createServer(params.server);
