90d20da963d33b19545b32b45b458c734bdec8f7
William TA <wta@student.42.fr>
2022-09-14 11:41:06 +0200

    chore: add test

--- test/client/app.spec.jsx
+++ test/client/app.spec.jsx
@@ -6,20 +6,12 @@ import React from 'react';
 import {
   render,
   screen,
-  fireEvent,
 } from './test-utils';
 
 import App from '../../src/client/containers/App';
 
-it('should render PlayerRegistrationInput', () => {
-  const utils = render(<App />);
-  //   const input = utils.getByLabelText('Choose a username');
+it('should render App', () => {
+  render(<App />);
 
-  //   const submitButton = screen.getByRole('button');
-
-  //   expect(submitButton).toBeDisabled();
-
-//   fireEvent.change(input, { target: { value: 'Bruce Wayne' } });
-//   expect(input.value).toBe('Bruce Wayne');
-//   expect(submitButton).not.toBeDisabled();
+  screen.getByRole('heading', { name: 'R E D T E T R I S' });
 });
